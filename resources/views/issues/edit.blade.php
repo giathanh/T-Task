d6d0b200ce04bb472973076ea878a022bfe0d07b
Chau Lai Gia Thanh <chaulaigiathanh@gmail.com>
2026-09-05 21:34:59 +0700

    feat: issue detail page

--- resources/views/issues/edit.blade.php
+++ resources/views/issues/edit.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex flex-col gap-4">
             <h2 class="text-xl font-medium leading-tight text-on-surface">
-                Thêm Task — {{ $project['name'] }}
+                Sửa issue #{{ $issue->id }} — {{ $project['name'] }}
             </h2>
 
             <x-project-nav :project="$project" active="issues" />
@@ -13,7 +13,9 @@
         <div class="mx-auto max-w-5xl sm:px-6 lg:px-8">
             @include('issues.partials.form', [
                 'project' => $project,
-                'action' => route('issues.store', $project['id']),
+                'issue' => $issue,
+                'action' => route('issues.update', [$project['id'], $issue->id]),
+                'method' => 'PUT',
                 'members' => $members,
                 'parentOptions' => $parentOptions,
                 'trackerOptions' => $trackerOptions,
