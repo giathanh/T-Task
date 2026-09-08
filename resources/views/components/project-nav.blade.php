d49093fec502719a7b33c8d8b802642fa6366624
Chau Lai Gia Thanh <chaulaigiathanh@gmail.com>
2026-09-08 09:39:45 +0700

    feat: calendar

--- resources/views/components/project-nav.blade.php
+++ resources/views/components/project-nav.blade.php
@@ -11,7 +11,7 @@
         ['key' => 'activity', 'label' => 'Activity', 'url' => route('projects.activity', $projectId)],
         ['key' => 'issues', 'label' => 'Issues', 'url' => route('issues.index', $projectId)],
         ['key' => 'wiki', 'label' => 'Wiki', 'url' => route('projects.wiki.index', $projectId)],
-        ['key' => 'calendar', 'label' => 'Calendar', 'url' => null],
+        ['key' => 'calendar', 'label' => 'Calendar', 'url' => route('projects.calendar', $projectId)],
     ];
 
     $baseClasses = 'inline-flex shrink-0 items-center rounded-full px-4 py-2 text-sm font-medium transition';
